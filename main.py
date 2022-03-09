21754e5ea8fa023c720ec09848f4b2afcb8f5110
jerrinm4 <jerrinm4@gmail.com>
2022-03-09 22:46:26 +0530

    initial working optimized

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pyzbar.pyzbar as pyzbar
 import pyttsx3
 import time
 import os.path
+import json
 from datetime import datetime
 
 def create_folder():
@@ -18,20 +19,15 @@ def create_folder():
     return f"./attentence/{year}/{month}/{datetime.now().strftime('%d-%m-%Y')}.txt"
 
 #fuction to add attentence
-def add(acc, y):
+def add(acc, s):
     global last_time
-    userData[acc] = y
+    userData[acc] = s
+    c_datetime=datetime.now().strftime('%d/%m/%Y %H:%M:%S')
     print(userData)
-    if y=="in":
-        with open (log_file,'a') as log:
-            log.write(f"ID: {acc}, Status: IN, DateTime: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")
-        text_speech.say("you are in ")
-        text_speech.runAndWait()
-    else:
-        with open(log_file, 'a') as log:
-            log.write(f"ID: {acc}, Status: OUT, DateTime: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")
-        text_speech.say("you are out ")
-        text_speech.runAndWait()
+    with open (log_file,'a') as log:
+        log.write(f"ID: {acc}, Status: {s}, DateTime: {c_datetime}\n")
+    text_speech.say(f"you are {s} ")
+    text_speech.runAndWait()
     last_time=time.time()
 #gobal variables
 last_read = "null" #to store late read data
@@ -40,31 +36,33 @@ userData=dict() #to store the current status
 log_file=create_folder()
 
 text_speech = pyttsx3.init()
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 while True:
-    data = ""
     _, frame = cap.read()
     decodedObjects = pyzbar.decode(frame)
     if decodedObjects:
+        print(decodedObjects)
+        data = ""
+        c_time=time.time()
         if decodedObjects[0].data != "":
             data = decodedObjects[0].data
         if last_read != data:
             read = str(data,'utf-8')
             if read in userData:
                     print("duplicate")
-                    if userData[read] == "in":
-                        add(read,"out")
+                    if userData[read] == "IN":
+                        add(read,"OUT")
                         last_read = data
                     else:
-                        add(read, 'in')
+                        add(read, 'IN')
                         last_read = data
             else:
-                add(read,"in")
+                add(read,"IN")
                 last_read = data
-        elif time.time()-last_time>30 and last_read != 'null':
-            print("wait")
+        elif c_time-last_time>30 and last_read != 'null':
             last_read='null'
         else:
+            print("wait")
             text_speech.say(f"wait {int(30-(time.time()-last_time))} seconds, to read again the same card ")
             text_speech.runAndWait()
 
