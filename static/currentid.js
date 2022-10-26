58f06a99ebfabaaa70287a0c1bf693a6b39af9ab
jerrinm4 <jerrinm4@gmail.com>
2022-10-26 23:06:58 +0550

    26-10-2022

--- static/currentid.js
+++ static/currentid.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
       $("#something").html(
         "<q><b>" +
           data["content"] +
-          "</q><br>-- <q>" +
+          "</q>-- <q>" +
           data["author"] +
           "</b></q>"
       );
