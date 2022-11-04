2fd2721f4cfb90c013ec71e6bfb01c8bf3e4d5af
jerrinm4 <jerrinm4@gmail.com>
2022-11-04 13:36:22 +0550

    04-11-2022

--- flaskserver.py
+++ flaskserver.py
@@ -91,7 +91,7 @@ cap = cv2.VideoCapture(0)
 # cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # this is the magic!
 #
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 450)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 337)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
 t1 = threading.Thread(target=addatt)
 t1.start()
